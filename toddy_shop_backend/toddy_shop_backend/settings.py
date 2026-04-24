f43a86bea1c256620b3a343264b7062b98d5e343
shamnad sherief <sshamnad03@gmail.com>
2026-04-24 08:06:34 +0530

    feat(core): implement strict APIResponse structure middleware guard

--- toddy_shop_backend/toddy_shop_backend/settings.py
+++ toddy_shop_backend/toddy_shop_backend/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.EnforceStandardResponseMiddleware',
 ]
 
 ROOT_URLCONF = 'toddy_shop_backend.urls'
